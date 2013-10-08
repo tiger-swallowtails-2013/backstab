f836436ac7291a92ec9e604b07b288038b48e9d6
Apprentice <apprentice@dbc30.local>
2013-10-08 10:12:59 -0700

    finsihed model tests

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -11,13 +11,22 @@ end
 
 describe Post do
 
-    let(:post) {FactoryGirl.build(:post)}
+  let(:post) {FactoryGirl.create(:post)}
+  context "validations" do
+    it "should be valid with title and content present" do
+      post.should be_valid
+    end
 
-  it "should be valid with title and content present" do
-    post.should be_valid
+    it "should not be valid without any content" do
+      FactoryGirl.build(:post, content: nil).should_not be_valid
+    end
+
+    it {should validate_presence_of(:content)}
   end
 
-  it "should not be valid without any content" do
-    FactoryGirl.build(:post, content: nil).should_not be_valid
-  end
+context "associations" do
+  it { should have_many(:comments) }
+  it { should belong_to(:parent)}
+end
+
 end
